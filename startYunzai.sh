2c8f18011ae172898f57916732bf18958505a3d6
Benson80 <81787444+Benson80@users.noreply.github.com>
2023-06-19 15:28:57 +0800

    Update startYunzai.sh

--- startYunzai.sh
+++ startYunzai.sh
@@ -1,9 +1,10 @@
+#! /bin/bash
 ## 转到Ubuntu目录
 cd ~/Termux-Linux/Ubuntu
 ./start-ubuntu.sh
 
 ## 启动redis
-redis-server –daemonize yes –save 900 1 –save 300 10
+redis-server --daemonize yes --save 900 1 --save 300 10
 
 ## 转到云崽目录
 cd Yunzai-Bot
