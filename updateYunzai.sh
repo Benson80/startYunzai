db015bda10e7d357b3280ac01ca04267844308fb
Benson80 <81787444+Benson80@users.noreply.github.com>
2023-06-21 12:53:26 +0800

    Update updateYunzai.sh

--- updateYunzai.sh
+++ updateYunzai.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-# 更新到最新的云崽
+## 更新到最新的云崽
 git remote set-url origin https://gitee.com/yoimiya-kokomi/Yunzai-Bot.git && git checkout main && git pull
+
+## 转到云崽目录
+cd Yunzai-Bot
 
 git reset --hard origin/main
 
