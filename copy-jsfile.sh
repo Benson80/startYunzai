94a10d0d9ea37cf2e092ec915a7a3dc1570d4efc
Benson80 <81787444+Benson80@users.noreply.github.com>
2023-06-22 19:10:06 +0800

    Add files via upload

---
+++ copy-jsfile.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# 复制俄轮脚本
+cd $(pwd)/Termux-Linux/Ubuntu/ubuntu-fs/root/Yunzai-Bot/plugins/y-tian-plugin/apps
+cp /sdcard/Download/js脚本/'el pro max.js' .
+
+# 复制电车脚本
+cd  $(pwd)/Termux-Linux/Ubuntu/ubuntu-fs/root/Yunzai-Bot/plugins/example
+cp /sdcard/Download/js脚本/tram.js .
