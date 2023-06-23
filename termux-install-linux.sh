6e5bed309f76369aba105be96c6349bbf4a556f7
Benson80 <81787444+Benson80@users.noreply.github.com>
2023-06-23 11:42:26 +0800

    Add files via upload

---
+++ termux-install-linux.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+## 依赖安装
+pkg install proot git python -y
+
+## 下载Linux安装脚本
+git clone https://gitee.com/Le-niao/termux-install-linux.git
+
+## 转到安装目录
+cd termux-install-linux
+
+## 安装和卸载Linux
+python termux-linux-install.py
